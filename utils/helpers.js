773f5a30a3042e41da9633895d2162cba62f7f1a
Alex Geis <siegxela@gmail.com>
2022-03-24 20:44:10 -0600

    removing date object declaration from helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
   format_date: (date) => {
-    const date = new Date(date);
+    // const date = new Date();
     console.log(date);
     console.log(date.getMonth());
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
