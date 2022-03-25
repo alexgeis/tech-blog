7ebc3c63f342aed9e2e44d36247acb6453e021b4
Alex Geis <siegxela@gmail.com>
2022-03-24 20:42:32 -0600

    attempt to fix JS date object error in helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,8 @@
 module.exports = {
   format_date: (date) => {
-    // const date = new Date(date);
+    const date = new Date(date);
+    console.log(date);
+    console.log(date.getMonth());
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   },
 };
